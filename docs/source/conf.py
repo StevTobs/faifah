01a9c85b6659a1a4074aa903076850138b2ad5a5
StevTobs <akanitk84@gmail.com>
2023-01-17 14:05:16 +0700

    Update to readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FaiFah'
+copyright = '2023, Akanit'
+author = 'Akanit'
 
 release = '0.1'
 version = '0.1.0'
